ffc0d63f12e45df96b5260ea2ad37bf509bde57f
lemoce <cerencio@yahoo.com.br>
2015-05-20 10:17:50 -0300

    Create memcmp.asm

---
+++ memcmp.asm
@@ -0,0 +1,31 @@
+        segment .data
+a       db      "This is fun"
+b       db      "This is not"
+
+        segment .text
+        global  _tmain
+        global  memcmp
+memcmp:
+        mov     rcx, rdx
+        repe    cmpsb
+        cmp     rcx, 0
+        jz      equal
+        movzx   eax, byte [rdi-1]
+        movzx   ecx, byte [rsi-1]
+        sub     rax, rcx
+        ret
+equal:
+        xor     eax, eax
+        ret
+
+_tmain:
+        push    rbp
+        mov     rbp, rsp
+        lea     rdi, [rel a]
+        lea     rsi, [rel b]
+        mov     edx, 11
+        call    memcmp
+        xor     eax, eax
+        leave
+        ret
+        
